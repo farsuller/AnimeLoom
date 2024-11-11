726d5b5e94e12668dcae0a23d75853b59926d6ba
farsuller <farsuller2020@gmail.com>
2024-11-11 18:54:11 +0800

    release version 10.1.0.0

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -8,11 +8,11 @@ object ProjectConfig {
     const val MIN_SDK = 26
     const val TARGET_SDK = 35
 
-    const val VERSION_CODE = 4
+    const val VERSION_CODE = 10
 
     const val MAJOR_VERSION = 1
     const val MINOR_VERSION = 0
     const val PATCH_VERSION = 0
 
-    const val GENERATE_LOCAL_ARCHIVE = false
+    const val GENERATE_LOCAL_ARCHIVE = true
 }
